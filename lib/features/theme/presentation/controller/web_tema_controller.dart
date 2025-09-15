4f9d26f8ee0925979ba17a7a611be9d9b09dd9f1
M Wahyu Ayesha <mine215567@gmail.com>
2025-09-15 15:30:40 +0700

    comment

--- lib/features/theme/presentation/controller/web_tema_controller.dart
+++ lib/features/theme/presentation/controller/web_tema_controller.dart
@@ -19,12 +19,13 @@ class WebTemaController extends GetxController {
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..addJavaScriptChannel(
         'ThemeChannel',
+        // jika menerima pesan dari JS
         onMessageReceived: (msg) async {
           print("📩 Dari JS: ${msg.message}");
           try {
-            final data = jsonDecode(msg.message);
-            final tema = TemaModel.fromJs(data);
-            await repository.updateTema(tema);
+            final data = jsonDecode(msg.message); // data dari JS
+            final tema = TemaModel.fromJs(data); // buat TemaModel dari data JS
+            await repository.updateTema(tema); // simpan tema ke database dengan rest API
             Get.snackbar(
               "Sukses",
               "Tema berhasil disimpan untuk Note ${tema.noteId}",
@@ -42,10 +43,11 @@ class WebTemaController extends GetxController {
   }
 
   Future<void> loadPage(String noteId, String userName) async {
-    final fullUrl =
-        "${Config.url_web}note_id=$noteId&user_name=$userName&_v=${DateTime.now().millisecondsSinceEpoch}";
+    final fullUrl = "${Config.url_web}note_id=$noteId&user_name=$userName&_v=${DateTime.now().millisecondsSinceEpoch}";
 
+    // bersihkan cache dulu agar menggunakan tampilan web terbaru
     await webViewController.clearCache();
+    // load webview controller dengan URL yang juga ada param noteId & userName
     webViewController.loadRequest(Uri.parse(fullUrl));
   }
 }
